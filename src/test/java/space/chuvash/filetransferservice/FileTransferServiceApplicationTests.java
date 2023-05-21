215b0db32af9d6562e403fe6b04445909ddef023
xinik <sergey20160@gmail.com>
2023-05-21 12:22:23 +0300

    Init repo

---
+++ src/test/java/space/chuvash/filetransferservice/FileTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package space.chuvash.filetransferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTransferServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
